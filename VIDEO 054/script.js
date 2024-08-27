b9be61a230ebc0fd9f1be80e3c847e207b253f20
SubhamSaha8688 <shadowmonarch8688@gmail.com>
2024-08-27 12:13:09 +0530

    Introduction to JavaScript & Installing Node.js

---
+++ VIDEO 054/script.js
@@ -0,0 +1,16 @@
+alert("Hello World!");
+
+console.log("Hey I am a console!");
+
+var a = prompt("Enter a number");
+var isTrue = confirm("Are you sure you want to leave")
+
+if (isTrue ) {
+    console.log("Computer is Blasting");
+} else {
+    console.log("Computer is Silent");
+}
+
+console.log("You entered: " + a);
+
+document.title = "Hey i am good";
