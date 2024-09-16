f98bf59fb6235975437e3243b66b9d80b753b045
SubhamSaha8688 <shadowmonarch8688@gmail.com>
2024-09-16 19:10:10 +0530

    JavaScript Arrays

--- VIDEO 063/index.js
+++ VIDEO 063/index.js
@@ -1,12 +1,21 @@
-let arr = [1,2,4,5,7]
+let arr = [1, 2, 4, 5, 7]
+//  Index  0, 1, 2, 3, 4
 
-console.log(arr);
-console.log(arr.length);
+arr[0] = 5666;
+// console.log(arr, typeof arr);
+// console.log(arr.length)
 
-for(let i = 0; i < arr.length; i++){
-    alert(arr[i]);
-    confirm(arr[i]);
-    let n = prompt("Enter the value at index " + i);
-    alert("n: " + arr[n]);
-}
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
 
+console.log(arr.toString())
+console.log(arr.join(" and "))
+
+
+ 
+// let numbers = [1, 2, 3, 4, 5] 
+// numbers.splice(1, 2)    
+// numbers.splice(1, 3)  
+// numbers.splice(1, 3, 222, 333) 
+// (4) [1, 222, 333, 5]
